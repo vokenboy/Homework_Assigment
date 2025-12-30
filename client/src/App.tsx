cddad4235e27cde349b35ab1a83fa8eab92b1378
Vilius Tamašauskas <viliustam36@gmail.com>
2025-12-30 23:52:11 +0200

    button z index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./views/Home";
 const App = () => {
     return (
         <>
-            <button className="fixed bg-white p-2 bottom-0 right-0 transition-all hover:bg-gray-200">
+            <button className="fixed bg-white p-2 bottom-0 right-0 transition-all hover:bg-gray-200 z-1">
                 Report a problem
             </button>
             <Home />
